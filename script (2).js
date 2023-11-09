9227fe09031b879cfc28182af9d2e37a1f89b425
Sock2009 <125119581+Sock2009@users.noreply.github.com>
2023-11-09 21:56:10 +0200

    Add files via upload

--- script (2).js
+++ script (2).js
@@ -28,7 +28,7 @@ window.addEventListener("load", function() {
 });
 function PagetoInfo() {
   window.scrollTo(0, 0);
-  window.location.href = 'sock2009.github.io/First/index (4).html';
+  window.location.href = 'https://sock2009.github.io/First/index (4).html';
 
   document.getElementById("fon-one").style.backgroundImage = "url('https://img3.akspic.ru/crops/0/6/3/5/7/175360/175360-sportkar-nissan_gt_r-legkovyye_avtomobili-nissan_skyline_gt_r-nissan-1080x1920.jpg')";
   document.getElementById("fon-one").style.width =  "100vw";
@@ -59,7 +59,7 @@ function PagetoInfo() {
 }
 function PagetoMoreInfo() {
   window.scrollTo(0, 0);
-
+  window.location.href = '  https://sock2009.github.io/NextInfo/moreinfo.html';
   document.getElementById("fon-one").style.backgroundImage = "url('https://img3.akspic.ru/crops/0/6/3/5/7/175360/175360-sportkar-nissan_gt_r-legkovyye_avtomobili-nissan_skyline_gt_r-nissan-1080x1920.jpg')";
   document.getElementById("fon-one").style.width =  "100vw";
   document.getElementById("fon-one").style.height =  "59cm";
@@ -90,7 +90,7 @@ function PagetoMoreInfo() {
 function PagetoMusics() {
   window.scrollTo(0, 0);
   //[p]
-  window.location.href = 'URL_іншої_сторінки';
+  window.location.href = 'https://sock2009.github.io/Second/music.html';
   
   document.getElementById("musicPlayerOne").style.display = "block";
   document.getElementById("musicPlayerTwo").style.display = "block";
